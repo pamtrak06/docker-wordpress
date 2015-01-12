01eff324ac4701c7335f8532baa69bf7c91287fb
pamtrak06 <pamtrak06@gmail.com>
2015-01-12 01:25:24 +0100

    update sh

--- start-wordpress.sh
+++ start-wordpress.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-docker run --name mysql-wp -e MYSQL_ROOT_PASSWORD=W0rdpress -d mysql --entrypoint=""
-docker run --name wordpress-srv --link mysql-wp:mysql -p 8080:80 -d wordpress --entrypoint=""
+docker run --entrypoint "" --name mysql-wp -e MYSQL_ROOT_PASSWORD=W0rdpress -d mysql
+docker run --entrypoint "" --name wordpress-srv --link mysql-wp:mysql -p 8080:80 -d wordpress
 
 mysql_id="$(docker ps -a | grep mysql-wp | cut -d ' ' -f1)"
 wordpress_id="$(docker ps -a | grep wordpress-srv | cut -d ' ' -f1)"
+
+docker restart $wordpress_id
 
 docker logs $mysql_id
 docker logs $wordpress_id
